74a5770f1d9d45ddb3c30098510c654aae9ae555
Abdou Samath YATTE <abdousamathyade@gmail.com>
2023-11-28 16:51:49 +0000

    french model

--- util.py
+++ util.py
@@ -137,4 +137,103 @@ def summarizer_lsa(texte_utilisateur, num_sentences=5):
     # Afficher le summary
     return text_summary
 
+#- ---------------------------------------------------FRENCH---------------------------------------------
 
+# Créer un text parser utilisant de tokenisation
+parser_french = PlaintextParser.from_string(texte_utilisateur, Tokenizer('french'))
+
+parser_french.document.sentences
+
+def french_summarizer_nltk(texte_utilisateur, num_sentences=5):
+
+    sentence_list = nltk.sent_tokenize(texte_utilisateur)
+
+    # Stopwords
+    stopwords = nltk.corpus.stopwords.words('french')
+    # Dictionnaire de fréquences des mots
+    word_frequencies = {}
+    for word in nltk.word_tokenize(texte_utilisateur):
+        if word not in stopwords:
+            if word not in word_frequencies.keys():
+                word_frequencies[word] = 1
+            else:
+                word_frequencies[word] += 1
+
+        # Fréquence maximale
+    maximum_frequency = max(word_frequencies.values())
+    # Calculer la fréquence pondérée
+    for word in word_frequencies.keys():
+        word_frequencies[word] = word_frequencies[word] / maximum_frequency
+
+        sentence_scores = {}
+    # Calculer le score de chaque phrase
+    for sent in sentence_list:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in word_frequencies.keys():
+                if len(sent.split(' ')) < 30:
+                    if sent not in sentence_scores.keys():
+                        sentence_scores[sent] = word_frequencies[word]
+                    else:
+                        sentence_scores[sent] += word_frequencies[word]
+
+    # Ordonner les phrases par pondération et recupérer les 10 premières phrases
+    summary_sentences_french = sorted(sentence_scores, key=sentence_scores.get, reverse=False)[:10]
+    # regrouper ensemble les phrases qui ont les poids les plus élévés
+    summary = ' '.join(summary_sentences_french)
+
+    # Afficher le résumé
+
+    return summary_sentences_french
+
+def french_summarizer_textrank(texte_utilisateur, num_sentences=5):
+# Initialiser le modèle
+    summarizer_textrank = TextRankSummarizer()
+
+    #tokeniser
+    parser_french = PlaintextParser.from_string(texte_utilisateur, Tokenizer('french'))
+
+    # Summariser en 5 phrases
+    summary = summarizer_textrank(parser_french.document, 5)
+
+    # Regrouper les phrases
+    text_summary = ""
+    for sentence in summary:
+        text_summary += str(sentence)
+
+    # Afficher le summary
+    return text_summary
+
+def french_summarizer_lexrank(texte_utilisateur, num_sentences=5):
+# Initialiser le modèle
+    summarizer_lexrank = LexRankSummarizer()
+
+    parser_french = PlaintextParser.from_string(texte_utilisateur, Tokenizer('french'))
+
+
+    # Summariser en 5 phrases
+    summary = summarizer_lexrank(parser_french.document, 5)
+
+    # Regrouper les phrases
+    text_summary = ""
+    for sentence in summary:
+        text_summary += str(sentence)
+        
+    # Afficher le summary
+    return text_summary
+
+def french_summarizer_lsa(texte_utilisateur, num_sentences=5):
+# Initialiser le modèle
+    summarizer_lsa = LsaSummarizer()
+
+    parser_french = PlaintextParser.from_string(texte_utilisateur, Tokenizer('french'))
+
+    # Summariser en 5 phrases
+    summary = summarizer_lsa(parser_french.document, 5)
+
+    # Regrouper les phrases
+    text_summary = ""
+    for sentence in summary:
+        text_summary += str(sentence)
+
+    # Afficher le summary
+    return text_summary
